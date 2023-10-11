d81397d14d398f0e705918e21b79f48e6000e5ab
torck82 Thai <torck82@gmail.com>
2023-10-11 02:19:14 +0000

    #42 finish implementation for terraform provider

--- main.tf
+++ main.tf
@@ -25,9 +25,9 @@ terraform {
 }
 
 provider "terratowns" {
-endpoint = "http://localhost:4567" 
-user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
-token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0" 
+  endpoint = "http://localhost:4567/api" 
+  user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
+  token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0" 
 }
 
 
@@ -42,3 +42,16 @@ token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 
 # }
 
+resource "terratowns_home" "home" {
+  name = "How to play Arcanum in 2023!"
+  description = <<DESCRIPTION
+Arcanum is a game from 2001 that shipped with alot of bugs.
+Modders have removed all the originals making this game really fun
+to play (despite that old look graphics). This is my guide that will
+show you how to play arcanum without spoiling the plot.
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+}
