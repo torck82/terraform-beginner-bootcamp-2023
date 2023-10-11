5d65ac1455d1e7ccf59898826ed210ee1af70bec
torck82 Thai <torck82@gmail.com>
2023-10-11 03:44:25 +0000

    #48 test our terra home on terratowns test town

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -3,7 +3,8 @@
 resource "aws_s3_bucket" "website_bucket" {
   # Bucket Naming Rules
   #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
- bucket = var.bucket_name
+ # we want to assign a radom bucket name
+ #bucket = var.bucket_name
  
 
  tags = {
--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -7,18 +7,18 @@ validation {
  }
 }
   
-  variable "bucket_name" {
-  description = "The name of the S3 bucket"
-  type        = string
+#   variable "bucket_name" {
+#   description = "The name of the S3 bucket"
+#   type        = string
 
-  validation {
-    condition     = (
-      can(regex("^[a-z0-9][a-z0-9-.]{1,61}[a-z0-9]$", var.bucket_name)) &&
-      length(regexall("^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$", var.bucket_name)) == 0
-    )
-    error_message = "The bucket_name must be between 3 and 63 characters in length, can contain only lowercase letters, numbers, hyphens, and periods, must start and end with a lowercase letter or number, and cannot be formatted as IP addresses."
-  }
-}
+#   validation {
+#     condition     = (
+#       can(regex("^[a-z0-9][a-z0-9-.]{1,61}[a-z0-9]$", var.bucket_name)) &&
+#       length(regexall("^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$", var.bucket_name)) == 0
+#     )
+#     error_message = "The bucket_name must be between 3 and 63 characters in length, can contain only lowercase letters, numbers, hyphens, and periods, must start and end with a lowercase letter or number, and cannot be formatted as IP addresses."
+#   }
+# }
 
 variable "index_html_filepath" {
   description = "Path to the index.html file"
