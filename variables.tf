368a22dcd29c38f0c9051644a677b2461984c19e
torck82 <54391362+torck82@users.noreply.github.com>
2023-10-12 02:15:06 -0400

    #50 fix issue with provider, setup two different towns (#51)

--- variables.tf
+++ variables.tf
@@ -1,33 +1,25 @@
-variable "teacherseat_user_uuid" {
-  type = string
-}
-
 variable "terratowns_endpoint" {
-  type = string
+ type = string
 }
 
 variable "terratowns_access_token" {
-  type = string
+ type = string
 }
 
-variable "bucket_name" {
-  type = string
-}
- 
-
- variable "index_html_filepath" {
-  type = string
+variable "teacherseat_user_uuid" {
+ type = string
 }
 
-variable "error_html_filepath" {
-  type = string
+variable "arcanum" {
+  type = object({
+    public_path = string
+    content_version = number
+  })
 }
 
-variable "content_version" {
-  type  = number
-}
-
-variable "assets_path" {
-  description = "Path to assets folder"
-  type = string
+variable "gaming" {
+  type = object({
+    public_path = string
+    content_version = number
+  })
 }
